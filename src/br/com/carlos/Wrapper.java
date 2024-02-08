399171184d80844a3794ff846046ce28a693213e
Carlos <carlosfremdling@gmail.com>
2024-02-08 00:59:34 -0300

    first commit

---
+++ src/br/com/carlos/Wrapper.java
@@ -0,0 +1,9 @@
+package br.com.carlos;
+
+public class Wrapper {
+
+    public static void main (String[] args) {
+        Integer idade = Integer.valueOf(30);
+        System.out.println(idade);
+    }
+}
