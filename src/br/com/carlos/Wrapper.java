8a80dfc212385d920a1b7dd739853c1c3175642f
Carlos <carlosfremdling@gmail.com>
2024-02-08 01:08:34 -0300

    Fix: ajuste da variável idade

--- src/br/com/carlos/Wrapper.java
+++ src/br/com/carlos/Wrapper.java
@@ -3,7 +3,8 @@ package br.com.carlos;
 public class Wrapper {
 
     public static void main (String[] args) {
-        Integer idade = Integer.valueOf(30);
-        System.out.println(idade);
+        int idade = 10;
+        Integer idade1 = Integer.valueOf(idade);
+        System.out.println(idade1);
     }
 }
